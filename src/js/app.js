7fbbdf4af033fb66882d44c0b5b8a5caa9a18c25
Alexandra Titova <auto-steklo@mail.ru>
2021-03-17 18:19:15 +0300

    added rules no-console

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,14 @@
 import characters from './person';
 import Team from './team';
 
-
-
 const team = new Team();
 team.addAll(...characters);
-
 
 for (const key of team) {
   console.log(key);
 }
 
-/*team.members.forEach(item =>{
+/* team.members.forEach(item =>{
 console.log(item);
 });
 */
--- src/js/team.js
+++ src/js/team.js
@@ -1,43 +1,43 @@
 export default class Team {
-    constructor() {
-        this.members = new Set();
-    }
+  constructor() {
+    this.members = new Set();
+  }
 
-    add(character) { // добавляет персонаж
-        if (this.members.has(character)) {
-            throw new Error('Такой персонаж уже есть');
-        }
-        this.members.add(character);
+  add(character) { // добавляет персонаж
+    if (this.members.has(character)) {
+      throw new Error('Такой персонаж уже есть');
     }
+    this.members.add(character);
+  }
 
-    addAll(...characters) { // добавляет произвольное количество персонажей
-        characters.forEach((character) => this.members.add(character));
-    }
+  addAll(...characters) { // добавляет произвольное количество персонажей
+    characters.forEach((character) => this.members.add(character));
+  }
 
-    toArray() { // конвертирует Set в массив
-        return Array.from(this.members);
-    }
+  toArray() { // конвертирует Set в массив
+    return Array.from(this.members);
+  }
 
   [Symbol.iterator]() {
-        let current = 0;
-        const members = this.toArray();
+    let current = 0;
+    const members = this.toArray();
+
+    return {
+      next() {
+        if (current < members.length) {
+          const value = members[current];
+          current += 1;
+
+          return {
+            done: false,
+            value,
+          };
+        }
 
         return {
-            next() {
-                if (current < members.length) {
-                    const value = members[current];
-                    current += 1;
-
-                    return {
-                        done: false,
-                        value,
-                    };
-                }
-
-                return {
-                    done: true,
-                };
-            },
+          done: true,
         };
-    }
+      },
+    };
+  }
 }
